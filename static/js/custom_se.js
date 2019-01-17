7390c4dbc50b41b4697975c963812c9b08c77be4
Fabien TREGUER <fabien.treguer@spacelephant.org>
2019-01-17 10:04:38 +0100

    feat: style adjustments

--- static/js/custom_se.js
+++ static/js/custom_se.js
@@ -10,4 +10,33 @@ jQuery(function ($) {
     })
     
 
+    // Smooth scroll to anchors
+    $('[href^="/#"]:not([href^="/#!"]), [href^="#"]:not([href^="#!"])').on('click', (e) => {
+        let target = $(e.currentTarget);
+        if (target.tagName === 'img') {
+          target = target.parent();
+        }
+        if (target.attr('href')) {
+          const anchor = $(target.attr('href').replace('/', ''));
+          animateScroll(anchor);
+        }
+      });
+
+      function animateScroll(anchor) {
+        const navHeader = $('.navbar-header');
+        const nav = $('#navigation');
+        let anchorOffset = anchor.offset();
+        let navHeight = navHeader.height() + parseInt(nav.css('padding-top'), 10) + parseInt(nav.css('padding-bottom'), 10);
+      
+        if (anchorOffset && navHeight) {
+          let scrollTop = anchorOffset.top - navHeight + 1;
+          $('html, body').animate(
+            {
+              scrollTop: scrollTop,
+            },
+            'slow',
+          );
+        }
+      }
+
 });
