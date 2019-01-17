fcdcc7f09cda6fd81a77ff18380fc259aedb8db8
Fabien TREGUER <fabien.treguer@spacelephant.org>
2019-01-17 12:00:06 +0100

    ci: ask confirmation before deploy

--- publish_ghpages.sh
+++ publish_ghpages.sh
@@ -26,8 +26,16 @@ hugo
 
 echo "Updating ${PUBLISH_BRANCH} branch"
 cd public
-echo www.spacelephant.org >> CNAME
-git add --all && git commit -m "Publishing to ${PUBLISH_BRANCH} (publish.sh)"
+echo www.spacelephant.org > CNAME
+git add --all && git commit -m "[CI]: Publishing master (publish.sh) | $(date -u '+%Y-%m-%d %H:%M:%S %Z')"
 
 echo "Push to ${PUBLISH_BRANCH} branch"
-git push origin $PUBLISH_BRANCH
+
+while true; do
+    read -p "Do you really want to deploy that version on ${PUBLISH_BRANCH}?" yn
+    case $yn in
+        [Yy]* ) git push origin ${PUBLISH_BRANCH}; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
